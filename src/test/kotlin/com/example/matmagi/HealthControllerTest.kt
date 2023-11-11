d557bf95343685b437c5412d37699c95aab402f5
henning myhre kvaløy <henning.myhre.kvaloey@sparebank1.no>
2023-11-11 11:46:58 +0100

    health endepunkt

---
+++ src/test/kotlin/com/example/matmagi/HealthControllerTest.kt
@@ -0,0 +1,23 @@
+package com.example.matmagi
+
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.get
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class HealthControllerTest {
+    @Autowired
+    private lateinit var mockMvc: MockMvc
+
+    @Test
+    fun `should get health` (){
+        mockMvc.get("/health").andExpect {
+            status { isOk() }
+            content { string("health") }
+        }
+    }
+}
